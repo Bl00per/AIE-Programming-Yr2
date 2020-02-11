2736fedf4bfd5a30965c8242285b95cb6ab79c86
Will <clouduniversemc@gmail.com>
2020-02-11 16:45:18 +1100

    Moving Camera to class

--- OpenGL Application/Camera.cpp
+++ OpenGL Application/Camera.cpp
@@ -1,1 +1,64 @@
 #include "Camera.h"
+
+
+void Camera::update(float a_delta_time)
+{
+	// 
+}
+
+void Camera::set_perspective(float fov, float aspect_ratio, float near, float far)
+{
+	projection_transform = glm::perspective(
+		glm::radians(fov),
+		aspect_ratio,
+		near,
+		far
+	);
+}
+
+void Camera::set_perspective_ortho(float left, float right, float bottom, float top, float near, float far)
+{
+	projection_transform = glm::ortho(
+		left,
+		right,
+		bottom,
+		top,
+		near,
+		far
+	);
+}
+
+void Camera::set_lookat(vec3 from, vec3 to, vec3 up)
+{
+	view_transform = glm::lookAt(
+		from,
+		to,
+		up
+	);
+}
+
+void Camera::set_position(vec3 position)
+{
+	// 
+}
+
+
+mat4 Camera::get_world_transform()
+{
+	return world_transform;
+}
+
+mat4 Camera::get_view()
+{
+	return view_transform;
+}
+
+mat4 Camera::get_projection()
+{
+	return projection_transform;
+}
+
+mat4 Camera::get_projection_view()
+{
+	return projection_view_transform;
+}
--- OpenGL Application/Camera.h
+++ OpenGL Application/Camera.h
@@ -1,5 +1,37 @@
 #pragma once
+#include "glfw3.h"
+#include "glm.hpp"
+#include "ext.hpp"
+
+using mat4 = glm::mat4;
+using vec3 = glm::vec3;
+
 class Camera
 {
+public:
+	mat4 world_transform;
+	mat4 view_transform;
+	mat4 projection_transform;
+	mat4 projection_view_transform;
+
+	void update(float a_delta_time);
+
+	// Setters
+	void set_perspective(float fov, float aspect_ratio, float near, float far);
+	void set_perspective_ortho(float left, float right, float bottom, float top, float near, float far);
+	void set_lookat(vec3 from, vec3 to, vec3 up);
+	void set_position(vec3 position);
+
+	// Getters
+	mat4 get_world_transform();
+	mat4 get_view();
+	mat4 get_projection();
+	mat4 get_projection_view();
+
+	//void update_projection_view_transform();
+
+private:
+	float delta_time;
+	float last_frame;
 };
 
